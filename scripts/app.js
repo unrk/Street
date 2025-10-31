23ef60f567702b151e802a03540d2abb78258777
memoir <153329824+unrk@users.noreply.github.com>
2025-10-31 11:29:48 +0100

    hmmmm

--- scripts/app.js
+++ scripts/app.js
@@ -49,7 +49,7 @@ async function initArrondissementPage() {
 // Charger les données depuis voie.json
 async function loadVoiesData() {
     try {
-        const response = await fetch('../datas/voie.json');
+        const response = await fetch('/streets/datas/voie.json');
         if (!response.ok) {
             throw new Error('Erreur de chargement du fichier');
         }
